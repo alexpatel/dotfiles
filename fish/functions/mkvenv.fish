8e2751463075abd791609982892e86765c42afcd
Alex Patel <alexanderpatel@college.harvard.edu>
2015-05-22 11:49:15 -0400

    source venv after creation

--- fish/functions/mkvenv.fish
+++ fish/functions/mkvenv.fish
@@ -1,6 +1,6 @@
 function mkvenv 
     if test (count $argv) = 1
-        virtualenv $HOME/venvs/$argv[1]
+        virtualenv $HOME/venvs/$argv[1]; and source $HOME/venvs/$argv[1]/bin/activate.fish
     else
         echo "Usage: mkvenv [virtualenv]"
     end
