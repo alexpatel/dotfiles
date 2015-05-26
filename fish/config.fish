a8b0c41be6d35e294b7a4ec284dd4e35a3854e4a
Alex Patel <alexanderpatel@college.harvard.edu>
2015-05-26 16:13:27 -0400

    Short vagrant aliases

--- fish/config.fish
+++ fish/config.fish
@@ -3,42 +3,43 @@ set fish_greeting ""
 alias bd="cd .."
 alias ...="cd ../.."
 alias -="cd -"
-alias d="cd ~/docs"
-alias c="cd ~/code"
-alias df="cd ~/code/dotfiles"
-alias dl="cd ~/downloads"
+alias d="cd ~/Documents"
+alias c="cd ~/Code"
+alias df="cd ~/Code/dotfiles"
+alias dl="cd ~/Downloads"
 # alias ls="ls --indicator-style=classify --group-directories-first --color"
 
 # git
 alias g="git"
-alias gs="git status"
 alias ga="git add"
-alias gl="git log --graph --abbrev-commit --decorate --date=relative --all"
+alias gb="git branch"
 alias gc="git commit"
 alias gd="git diff"
 alias gco="git checkout"
+alias gl="git log --graph --abbrev-commit --decorate --date=relative --all"
 alias gps="git push"
 alias gpl="git pull"
+alias gra="git rebase --abort"
 alias grb="git rebase -i"
 alias grc="git rebase --continue"
-alias gra="git rebase --abort"
+alias gs="git status"
 
 # CVS
 set -gx CVS_RSH ssh 
 set -gx CVSROOT $HOME/Code/gnu/www
 
 # vagrant
-alias vup="vagrant up"
-alias vhalt="vagrant halt"
+alias vu="vagrant up"
+alias vh="vagrant halt"
 alias vssh="vagrant ssh"
 alias vr="vagrant reload"
-alias vdestroy="vagrant destroy"
-alias vreload="vagrant reload"
+alias vds="vagrant destroy"
+alias vrl="vagrant reload"
 
 alias manage="bash vagrant_manage.sh"
 
 # python
-alias python="python2"
+#alias python="python2"
 
 # os161
 set CS161 $HOME/docs/cs161
