4bbd27b144466479ac45236b980de59c6cd1bdbd
Alex Patel <alexanderpatel@college.harvard.edu>
2015-07-18 14:21:38 -0400

    Fix virtualenv Fish aliases

--- fish/functions/activate.fish
+++ fish/functions/activate.fish
@@ -1,6 +1,6 @@
 function activate
     if test (count $argv) = 1
-        source $HOME/venvs/$argv[1]/bin/activate.fish
+        source $HOME/.venvs/$argv[1]/bin/activate.fish
     else
         echo "Usage: activate [virtualenv]"
     end
