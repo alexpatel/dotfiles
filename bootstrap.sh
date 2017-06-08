2f0a9e4684542796ced8a4466afadd1b6fa57d42
Alex Patel <patelalex02@gmail.com>
2017-06-08 19:32:05 -0400

    update bootstrap script for osx

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,20 +1,17 @@
 #!/bin/bash
 
+# install configuration files to desktop
+
 declare -A map=(
-	["i3config"]="$HOME/.i3/config"
-	["slock.sh"]="$HOME/.i3/slock.sh"
-	["i3status.conf"]="$HOME/.i3status.conf"
 	["vimperatorrc"]="$HOME/.vimperatorrc"
 	["vimrc"]="$HOME/.vimrc"
 	["vim"]="$HOME/.vim"
-	["xbindkeysrc"]="$HOME/.xbindkeysrc"
-	["xinitrc"]="$HOME/.xinitrc"
-	["Xresources"]="$HOME/.Xresources"
-	["fish"]="$HOME/.config/fish"
-	["fish_config"]="$HOME/.config/fish/config.fish"
-	["dunstrc"]="$HOME/.config/dunst/dunstrc"
+	["bashrc"]="$HOME/.bashrc"
+	["bash_aliases"]="$HOME/.bash_aliases"
+	["tmux.conf"]="$HOME/.tmux.conf"
 )
 
-for f in "${!map[@]}"; do ln -s "$(pwd)/$f" ${map[$f]}; done
-ln -s "user-dirs.dirs" $HOME/.config/user-dirs.dirs
-chmod +x $HOME/.i3/slock.sh
+for f in "${!map[@]}"
+do 
+    ln -s "$(pwd)/$f" ${map[$f]}
+done
