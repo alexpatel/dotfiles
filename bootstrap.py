42a32c3fc10b3cf2fb309851e92de6dcc8277beb
Alex Patel <patelalex02@gmail.com>
2017-08-08 21:17:26 -0400

    port bootstrap.sh to python

---
+++ bootstrap.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+import os
+
+conf_map = {
+	"vimperatorrc": "$HOME/.vimperatorrc",
+	"vimrc": "$HOME/.vimrc",
+	"vim": "$HOME/.vim",
+	"bashrc": "$HOME/.bashrc",
+	"bash_aliases": "$HOME/.bash_aliases",
+	"tmux": "$HOME/.tmux.conf",
+	"hgrc": "$HOME/.hgrc",
+	"hgignore": "$HOME/.hgignore",
+}
+
+for source, dest in conf_map.iteritems():
+	os.system('ln -s "$(pwd)/{}" {}'.format(source, dest))
